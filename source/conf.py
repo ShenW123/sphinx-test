57b09efd8e7ea4bb9e42479dfe0fc68e68ce71a7
ShenW123 <shen.w.sw@gmail.com>
2016-01-23 16:54:11 -0500

    fix bug

--- source/conf.py
+++ source/conf.py
@@ -32,7 +32,7 @@ import sphinx_rtd_theme
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.mathjax'
+extensions = ['sphinx.ext.mathjax',
 			  'juliadoc.julia',
               'juliadoc.jldoctest',
               'juliadoc.jlhelp']
